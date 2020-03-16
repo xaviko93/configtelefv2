fb5c0947e15a2979fc65a1575930efd0cd03c988
xaviko93 <mcvilo25@gmail. om>
2020-03-17 00:56:35 +0100

    buscador notarias hecho

---
+++ configurador_tlf_V2/configurador_tlf_V2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace configurador_tlf_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buscadornotaria = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.gridnotarias = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gridextensiones = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridnotarias)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridextensiones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(566, 535);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Progreso teléfono:";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(569, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(534, 483);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(856, 535);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Progreso total:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(665, 526);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(163, 32);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(937, 525);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(166, 33);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nombre notaría:";
+            // 
+            // buscadornotaria
+            // 
+            this.buscadornotaria.Location = new System.Drawing.Point(137, 19);
+            this.buscadornotaria.Name = "buscadornotaria";
+            this.buscadornotaria.Size = new System.Drawing.Size(305, 20);
+            this.buscadornotaria.TabIndex = 7;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(466, 17);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 8;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gridnotarias
+            // 
+            this.gridnotarias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridnotarias.Location = new System.Drawing.Point(39, 46);
+            this.gridnotarias.Name = "gridnotarias";
+            this.gridnotarias.Size = new System.Drawing.Size(502, 93);
+            this.gridnotarias.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Teléfonos notaría (Extensiones):";
+            // 
+            // gridextensiones
+            // 
+            this.gridextensiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridextensiones.Location = new System.Drawing.Point(39, 170);
+            this.gridextensiones.Name = "gridextensiones";
+            this.gridextensiones.Size = new System.Drawing.Size(502, 120);
+            this.gridextensiones.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 586);
+            this.Controls.Add(this.gridextensiones);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gridnotarias);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.buscadornotaria);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridnotarias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridextensiones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox buscadornotaria;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.DataGridView gridnotarias;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView gridextensiones;
+    }
+}
+
---
+++ configurador_tlf_V2/configurador_tlf_V2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace configurador_tlf_V2
+{
+
+    public partial class Form1 : Form
+    {
+
+        public String notariaabuscar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection Conexion = new MySqlConnection("server=remotemysql.com; database=wWWHH1xcMX; Uid=wWWHH1xcMX; pwd=MmxyP2R8ey");
+        DataSet ds;
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notariaabuscar = buscadornotaria.Text.ToString();
+            gridnotarias.DataSource = "<<>>";
+
+
+
+            Conexion.Open();
+            MySqlCommand mostrar = new MySqlCommand("SELECT NomNotaria, IPCentralita, MascaraRed, PuertaEnlace FROM centralita WHERE NomNotaria LIKE '%" + notariaabuscar + "%'", Conexion);
+
+            MySqlDataAdapter m_datos = new MySqlDataAdapter(mostrar);
+            ds = new DataSet();
+            m_datos.Fill(ds);
+
+            gridnotarias.DataSource = ds.Tables[0];
+            Conexion.Close();
+        }
+
+
+    }
+}
